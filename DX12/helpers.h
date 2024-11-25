00f9bdcbb9168f98e00ef7b7df89aad0c88ecea6
João Patrício <joaopat98@gmail.com>
2024-11-25 00:36:24 +0000

    Initial DX12 boilerplate (cornflower blue window: https://www.3dgep.com/learning-directx-12-1/)

---
+++ DX12/helpers.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "MinWindows.h" // For HRESULT
+#include <exception>
+
+// From DXSampleHelper.h 
+// Source: https://github.com/Microsoft/DirectX-Graphics-Samples
+inline void ThrowIfFailed(HRESULT hr)
+{
+    if (FAILED(hr))
+    {
+        throw std::exception();
+    }
+}
